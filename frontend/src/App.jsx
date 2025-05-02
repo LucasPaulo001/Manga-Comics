5f3b26218298e975e971dc67274f6fe0366b1d6a
Lucas Paulo <lp152659@gmail.com>
2025-05-02 17:00:56 -0300

    Iniciando setup do frontend, configurações iniciais e importações de módulos a serem utilizados

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+//Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path='/' element={} /> */}
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  )
+}
+
+export default App
